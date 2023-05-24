7459242f0102712a0b65d51dd8f7be1e0dc0a68c
rodgersxy <rodgersny99@gmail.com>
2023-05-24 07:04:40 -0700

    _opcode.c

---
+++ _opcode.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+/**
+* op_selector - for selecting options
+* @stack: for stack
+* @line_number: number of lines
+* Return: Nothing
+*/
+void op_selector(stack_t **stack, unsigned int line_number)
+{
+	int target = 0, idx = 0;
+	char *token = strdup(free_memory.line);
+
+	instruction_t op[] = {{"push", push}, {"pint", pint}, {"pall", pall},
+	{"pop", pop}, {"swap", swap}, {"sub", sub}, {"nop", nop}, {"div", div},
+	{"mod", mod}, {"mul", mul}, {"add", add}, {"pchar", pchar}, {"pstr", pstr},
+	{"rotl", rotl}, {NULL, NULL}};
+
+	free_memory.temp = token;
+	token = strtok(token, DELIMITERS);
+	if (token == NULL)
+	{
+		free(free_memory.temp)
+		return;
+	}
+	else if (token[0] != '\0' && token[0] == '#')
+	{
+		free(free_memory.temp)
+		return;
+	}
+	/*iterate*/
+	while (op[idx].opcode != NULL)
+	{
+		if (strcmp(token, op[idx].opcode) == 0)
+		{
+			target = 1;
+			(op[idx].f)(stack, line_number);
+			break;
+		}
+		idx++;
+	}
+	if (target == 0)
+	{
+		fprintf(stderr, "L%u: unknown institution %s\n", line_number, token);
+		free_file();
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	free(free_memory.temp);
+}
