4c5861405b4e70a0fe210d89a286d3010f4b4542
Pontuagi <benepontuagi@gmail.com>
2023-05-24 07:24:15 -0700

    main.c

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include "monty.h"
+
 carry free_memory;
+
 /**
 * main - entry point
-* @argc: argument count
-* @argv: argument value(number of lines)
-* Return: 0 if success else 1
+* @ac: argument count
+* @av: argument value(number of lines)
 *
+* Return: 0 if success else 1
 */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
 	unsigned int line_number = 0;
 	stack_t *stack = NULL;
 	size_t len = 0;
 	ssize_t line_size;
-	char *filename = argv[1];
+	char *filename = av[1];
 
 	free_memory.fp = fopen(filename, "r");
 	if (argc != 2)
@@ -23,7 +25,7 @@ int main(int argc, char **argv)
 	}
 	if (!free_memory.fp)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 
@@ -36,7 +38,7 @@ int main(int argc, char **argv)
 	}
 
 	free(free_memory.line);
-	kai(&stack);
+	free_stack(&stack);
 	fclose(free_memory.fp);
 	return (EXIT_SUCCESS);
 }
