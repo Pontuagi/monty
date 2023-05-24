f38068e8222f1bacdf6b0b7653cc7cc7ddba3446
Pontuagi <benepontuagi@gmail.com>
2023-05-24 05:01:08 -0700

    removing file

--- pint_opcode.c
+++ pint_opcode.c
@@ -10,12 +10,4 @@
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-	/*checks if the stack is empty, else prints value*/
-	if (stack == NULL || *stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	printf("%d\n", (*stack)->n);
 }
--- push_pall.c
+++ push_pall.c
@@ -10,32 +10,6 @@
   */
 void opcode_push(stack_t **stack, unsigned int line_number, char *value)
 {
-	int no;
-	stack_t *new_node;
-
-	if (value == NULL || *value == '\0')
-	{
-		fprintf(stderr, "L%d: usage : push interger\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	no = atoi(value);
-	if (no == 0 && *value != '0')
-	{
-		fprintf(stderr, "Error: Memory allocation failed\n");
-		exit(EXIT_FAILURE);
-	}
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: Memory allocation failed\n");
-		exit(EXIT_FAILURE);
-	}
-	new_node->n = no;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
-	*stack = new_node;
 }
 
 /**
@@ -47,13 +21,4 @@ void opcode_push(stack_t **stack, unsigned int line_number, char *value)
   */
 void opcode_pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current = *stack;
-
-	(void)line_number;
-
-	while (current != NULL)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-	}
 }
