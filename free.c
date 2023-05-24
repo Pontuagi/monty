99754d87b3b80bc34d83136725e3bcf54eef6033
Pontuagi <benepontuagi@gmail.com>
2023-05-24 06:44:41 -0700

    free_function

---
+++ free.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+  * free_file - free memory space
+  */
+void free_file(void)
+{
+	free(free_memory.tmp);
+	free(free_memory.line);
+	fclose(free_memory.fp);
+}
+
+/**
+  * free_memory - free memory space
+  * @stack: head of a stack
+  */
+void free_memory(stack_t **stack)
+{
+	stack_t *temp;
+
+	while ((*stack) != NULL)
+	{
+		temp = *stack;
+		*stack = (*stack)->next;
+		free(temp);
+	}
+	free((*stack));
+}
--- push_pall.c
+++ push_pall.c
@@ -4,12 +4,49 @@
   * opcode_push - push an element into a stack
   * @stack: top of stack
   * @line_number: line number in stack
-  * @value: element to push to stack
   *
   * Return: nothing
   */
-void opcode_push(stack_t **stack, unsigned int line_number, char *value)
+void opcode_push(stack_t **stack, unsigned int line_number)
 {
+	char *tok;
+	stack_t *new_node = NULL;
+	int no = 0;
+
+	new_tok = strtok(free_memory.line, DELIMITERS);
+	if (new_tok == NULL)
+	{
+		jutsu_liber();*
+		kai(stack);*
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	while (tok[no] != '\0')
+	{
+		if ((tok[no] < '0' || tok[no] > '9') && tok[no] != '-')
+		{
+		jutsu_liber();*
+		kai(stack);*
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+		}
+		no++;
+	}
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = atoi(tok);
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	if (*stack != NULL)
+	{
+		new_next = *stack;
+		(*stack)->new;
+	}
+	*stack = new;
 }
 
 /**
@@ -21,4 +58,14 @@ void opcode_push(stack_t **stack, unsigned int line_number, char *value)
   */
 void opcode_pall(stack_t **stack, unsigned int line_number)
 {
+	stack_t *printer = stack;
+	(void)line_number;
+
+	if (stack == NULL || *stack == NULL)
+		return;
+	while (printer != NULL)
+	{
+		printf("%d\n", print_stack->n);
+		print_stack = print_stack->next;
+	}
 }
